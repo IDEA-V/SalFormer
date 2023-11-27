15a7f9f43c7096f4e03feb213ecd53e15bc9da21
Yao Wang <yao.wang@vis.uni-stuttgart.de>
2023-11-27 11:33:54 +0100

    fix: name

--- trainning_llama_freeze.py
+++ trainning_llama_freeze.py
@@ -21,31 +21,19 @@ from torch.utils.tensorboard import SummaryWriter
 
 def trainning_llama(device, KL, CC, NSS, LR):
     writer = SummaryWriter(comment=f"KL_{KL}_CC_{CC}_NSS_{NSS}")
-
     number_epoch = 200
     eps=1e-6
     batch_size = 16
 
-    # vit = timm.create_model('xception41p.ra3_in1k', pretrained=True)
-    # data_config = timm.data.resolve_model_data_config(vit)
-    # img_transform_no_augment = timm.data.create_transform(**data_config, is_training=True)
-
     train_set = ImagesWithSaliency("data/train.npy", dtype=torch.float32)
     val_set = ImagesWithSaliency("data/val.npy", dtype=torch.float32)
-    test_set = ImagesWithSaliency("data/test.npy", dtype=torch.float32)
     train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True, collate_fn=padding_fn)
-    # vali_dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=True, collate_fn=padding_fn)
-    vali_dataloader = DataLoader(test_set, batch_size=batch_size, shuffle=True, collate_fn=padding_fn)
+    vali_dataloader = DataLoader(val_set, batch_size=batch_size, shuffle=True, collate_fn=padding_fn)
 
-
-    # vit = ViTModel.from_pretrained("google/vit-base-patch16-224-in21k")
     vit = SwinModel.from_pretrained("microsoft/swin-tiny-patch4-window7-224")
-    # vit = CvtModel.from_pretrained("microsoft/cvt-13")
-    # vit = SwinModel.from_pretrained("microsoft/swin-base-patch4-window12-384")
     print('SwinModel loaded')
 
     llama = LlamaModel.from_pretrained("Enoch/llama-7b-hf", low_cpu_mem_usage=True)
-    # # llama = LlamaModel.from_pretrained("Enoch/llama-7b-hf")
     print("llama loaded")
     # bloom = BloomModel.from_pretrained("bigscience/bloom-3b")
     # print('BloomModel loaded')
@@ -106,7 +94,7 @@ def trainning_llama(device, KL, CC, NSS, LR):
                         'model_state_dict': model.state_dict(),
                         'optimizer_state_dict': optimizer.state_dict(),
                         'loss': loss
-                    }, f'./ckpt/model_llama_10kl_5cc_2nss_{epoch}.tar')
+                    }, f'./ckpt/model_llama_{KL}kl_{CC}cc_{NSS}nss_{epoch}.tar')
 
                 model.eval()
                 test_loss = 0
@@ -119,18 +107,18 @@ def trainning_llama(device, KL, CC, NSS, LR):
 
                         if y.shape[0] == batch_size:
                             for i in random.sample(range(0, y.shape[0]), 3):
-                                save_image(y[i].type(torch.float32), f'./results_llm/test/epoch{epoch}_batch{batch}_{i}.png')
-                                save_image(hm[i].type(torch.float32), f'./results_llm/test/epoch{epoch}_batch{batch}_{i}_truth.png')
+                                save_image(y[i].type(torch.float32), f'./results_llm/val/epoch{epoch}_batch{batch}_{i}.png')
+                                save_image(hm[i].type(torch.float32), f'./results_llm/val/epoch{epoch}_batch{batch}_{i}_truth.png')
 
                         test_kl += kl.item()/len(vali_dataloader)
                         test_cc += cc.item()/len(vali_dataloader)
                         test_nss += nss.item()/len(vali_dataloader)
                 model.train(True)
                 print("Testing: loss ", test_loss, "kl ", test_kl, "cc ", test_cc, "nss ", test_nss)
-                writer.add_scalar('Loss/test', test_loss, epoch)
-                writer.add_scalar('Loss/test/kl', test_kl, epoch)
-                writer.add_scalar('Loss/test/cc', test_cc, epoch)
-                writer.add_scalar('Loss/test/nss', test_nss, epoch)
+                writer.add_scalar('Loss/val', test_loss, epoch)
+                writer.add_scalar('Loss/val/kl', test_kl, epoch)
+                writer.add_scalar('Loss/val/cc', test_cc, epoch)
+                writer.add_scalar('Loss/val/nss', test_nss, epoch)
             n_iter += 1
 
 
