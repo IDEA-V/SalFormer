dbf4d26d003791e8053355a79b18630d22fc84f4
Yao Wang <yao.wang@vis.uni-stuttgart.de>
2023-11-30 15:09:40 +0100

    fix: finoata augmentation

--- preprocess.py
+++ preprocess.py
@@ -1,36 +1,12 @@
 import random
 random.seed(666)
-
-import os
-os.environ['TORCH_HOME'] = '/projects/wang/.cache/torch'
-os.environ['TRANSFORMERS_CACHE'] = '/projects/wang/.cache'
-
-# import torch
+from env import *
 from torchvision import transforms
-# from torchvision.utils import save_image
-# from transformers import BertModel, SwinModel
-# from torch.utils.data import DataLoader
-# from torch.nn.utils.rnn import pad_sequence
-
-# from dataset import ImagesWithSaliency
-# from model_swin import SalFormer
-
-from torch.utils.tensorboard import SummaryWriter
-# from utils import padding_fn, log_softmax2d, softmax2d, inference
-
 import json
 from PIL import Image
 import numpy as np
 from torchvision.io import read_image
-from pathlib import Path
 from tqdm import tqdm
-
-writer = SummaryWriter()
-
-device = 'cuda:0'
-number_epoch = 220
-eps=1e-10
-batch_size = 32
 
 img_transform = transforms.Compose([
     transforms.ToTensor(),
@@ -56,9 +32,6 @@ hm_transform = transforms.Compose([
 ])
 
 
-# dataset_path = './SalChartQA'
-dataset_path = '/datasets/internal/datasets_wang/SalChartQA/SalChartQA-split'
-
 
 def preprocess_dataset(img_folder, fixation_map_folder, heat_map_folder):
     datas = []
@@ -69,19 +42,22 @@ def preprocess_dataset(img_folder, fixation_map_folder, heat_map_folder):
         id = img_name.split(".")[0]
 
         q0 = qa[img_name]['Q0']
-        img = img_transform(Image.open(f"{img_folder}/{img_name}").convert("RGB"))
+        img = img_transform_no_augment(Image.open(f"{img_folder}/{img_name}").convert("RGB"))
         fixation = fix_transform(read_image(f"{fixation_map_folder}/{id}_Q0.png"))
         hm = hm_transform(read_image(f"{heat_map_folder}/{id}_Q0.png"))
         datas.append([img, q0, fixation, hm, f"{id}_Q0.png"])
 
         q1 = qa[img_name]['Q1']
-        img = img_transform(Image.open(f"{img_folder}/{img_name}").convert("RGB"))
+        img = img_transform_no_augment(Image.open(f"{img_folder}/{img_name}").convert("RGB"))
         fixation = fix_transform(read_image(f"{fixation_map_folder}/{id}_Q1.png"))
         hm = hm_transform(read_image(f"{heat_map_folder}/{id}_Q1.png"))
         datas.append([img, q1, fixation, hm, f"{id}_Q1.png"])
     np.save(f"data/{img_folder.split('/')[-3]}.npy", np.array(datas, dtype=object))
 
 if __name__ == '__main__':
+    # dataset_path = './SalChartQA'
+    dataset_path = '/datasets/internal/datasets_wang/SalChartQA/SalChartQA-split'
+
     preprocess_dataset(f"{dataset_path}/train/raw_img/", f"{dataset_path}/train/saliency_all/fix_maps/", f"{dataset_path}/train/saliency_all/heatmaps/")
     preprocess_dataset(f"{dataset_path}/val/raw_img/", f"{dataset_path}/val/saliency_all/fix_maps/", f"{dataset_path}/val/saliency_all/heatmaps/")
     preprocess_dataset(f"{dataset_path}/test/raw_img/", f"{dataset_path}/test/saliency_all/fix_maps/", f"{dataset_path}/test/saliency_all/heatmaps/")
